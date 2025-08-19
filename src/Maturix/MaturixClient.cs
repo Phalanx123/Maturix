2e7eb79af74313d6a37347c259b225f22268c617
Andre Ferreira <AFerreira@PFWork.com.au>
2025-08-19 13:31:12 +1000

    Initial commit

--- src/Maturix/MaturixClient.cs
+++ src/Maturix/MaturixClient.cs
@@ -1,5 +1,10 @@
+using System;
+using System.Collections.Generic;
 using System.Net;
+using System.Net.Http;
 using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using Maturix.Models;
 using OneOf;
--- tests/Maturix.Tests/MaturixClientTests.cs
+++ tests/Maturix.Tests/MaturixClientTests.cs
@@ -1,4 +1,8 @@
+using System;
 using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.Extensions.Logging.Abstractions;
 using Moq;
 using Moq.Protected;
